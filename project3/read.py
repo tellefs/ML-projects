69bc64dbcd1f2dba4c0803b832410a002873ca04
jvevik <j.e.vevik@fys.uio.no>
2020-12-08 19:47:42 +0100

    gridsearch for decision tree

--- project3/read.py
+++ project3/read.py
@@ -31,10 +31,10 @@ fit = Fitting(bind_eng)
 
 #fit.OLS()
 #fit.XGB()
-fit.decision_tree(depth=10,lamb=0.0)
+#fit.decision_tree(depth=10,lamb=0.0)
 
 
-print("Train R2 score:")
+'''print("Train R2 score:")
 print(R2(bind_eng.z_train, fit.z_tilde))
 
 print("Train MSE score:")
@@ -44,8 +44,48 @@ print("Test R2 score:")
 print(R2(bind_eng.z_test, fit.z_predict))
 
 print("Test MSE score:")
-print(MSE(bind_eng.z_test, fit.z_predict))
+print(MSE(bind_eng.z_test, fit.z_predict))'''
 
+depth_values = np.linspace(1,10,10)
+lambda_values = np.hstack((np.array([0.0]), np.logspace(-6,-1,6)))
+
+
+filename_1 = 'Files/DecisionTree_test_MSE.txt'
+filename_2 = 'Files/DecisionTree_test_R2.txt'
+filename_3 = 'Files/DecisionTree_train_MSE.txt'
+filename_4 = 'Files/DecisionTree_train_R2.txt'
+
+f_1 = open(filename_1, "w")
+f_1.write("lambda   depth  MSEtest\n")
+f_1.close()
+f_2 = open(filename_2, "w")
+f_2.write("lambda   depth  R2test\n")
+f_2.close()
+f_3 = open(filename_3, "w")
+f_3.write("lambda   depth  MSEtrain\n")
+f_3.close()
+f_4 = open(filename_4, "w")
+f_4.write("lambda   depth  R2train\n")
+f_4.close()
+
+f_1 = open(filename_1, "a")
+f_2 = open(filename_2, "a")
+f_3 = open(filename_3, "a")
+f_4 = open(filename_4, "a")
+
+for i, depth in enumerate(depth_values):
+    for j, lamb in enumerate(lambda_values):
+        fit.decision_tree(depth=depth,lamb=lamb)
+
+        f_1.write('{0} {1} {2}\n'.format(lamb, depth, MSE(bind_eng.z_test, fit.z_predict)))
+        f_2.write('{0} {1} {2}\n'.format(lamb, depth, R2(bind_eng.z_test, fit.z_predict)))
+        f_3.write('{0} {1} {2}\n'.format(lamb, depth, MSE(bind_eng.z_train, fit.z_tilde)))
+        f_4.write('{0} {1} {2}\n'.format(lamb, depth, R2(bind_eng.z_train, fit.z_tilde)))
+
+f_1.close()
+f_2.close()
+f_3.close()
+f_4.close()
 
 
 # Rescaling the data back
