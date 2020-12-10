ed80d9b49eceba7e8536352fccc0eeeb0d82d836
Tellef Storebakken <tellefstorebakken@gmail.com>
2020-12-10 12:00:01 +0100

    optimized the learning rate for XGBoost

--- project3/XGB_analysis.py
+++ project3/XGB_analysis.py
@@ -32,7 +32,7 @@ bind_eng.test_train_split(0.2)
 fit = Fitting(bind_eng)
 
 depth_values = np.linspace(1,10,10)
-lambda_values = np.hstack((np.array([0.0]), np.logspace(-6,0,7)))
+lambda_values = np.hstack((np.array([0.0]), np.logspace(-4,2,7)))
 
 filename_1 = 'Files/XGB_test_MSE.txt'
 filename_2 = 'Files/XGB_test_R2.txt'
@@ -60,7 +60,7 @@ f_4 = open(filename_4, "a")
 for i, depth in enumerate(depth_values):
     for j, lamb in enumerate(lambda_values):
         depth = int(depth)
-        fit.XGB(max_depth=depth,reg_lambda=lamb)
+        fit.XGB(max_depth=depth,reg_lambda=lamb, learning_rate=0.882)#found this to be the best learning rate
 
         f_1.write('{0} {1} {2}\n'.format(lamb, depth, MSE(bind_eng.z_test, fit.z_predict)))
         f_2.write('{0} {1} {2}\n'.format(lamb, depth, R2(bind_eng.z_test, fit.z_predict)))
@@ -72,6 +72,27 @@ f_2.close()
 f_3.close()
 f_4.close()
 
+#part of script to find optimal learning rate
+"""
+eta_values = np.linspace(0.881, 0.883, 10)
+R2_score = 0
 
+for i, depth in enumerate(depth_values):
+    for j, lamb in enumerate(lambda_values):
+        for k, eta in enumerate(eta_values):
+        	depth = int(depth)
+        	fit.XGB(max_depth=depth,reg_lambda=lamb, learning_rate=eta)
+        	new_R2_score = R2(bind_eng.z_test, fit.z_predict)
+        	if new_R2_score>R2_score:
+        		best_eta = eta
+        		best_lambda = lamb
+        		best_depth = depth
+        		R2_score=new_R2_score
+
+print(R2_score)
+print(best_depth)
+print(best_lambda)
+print(best_eta)
+"""
 # Rescaling the data back
 bind_eng.data_rescaling()
